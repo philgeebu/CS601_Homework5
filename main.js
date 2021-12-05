1fb7098422cbb30e331161be427a89aac57b0853
Phil Gee <philgeejunk@gmail.com>
2021-12-05 18:42:31 -0500

    refactoring, wip. need to style

--- main.js
+++ main.js
@@ -6,54 +6,61 @@ const showDegreeInfo = () => {
             } else throw new Error('Fail');
         })
         .then(data => {
-            createDegreeInfoTable(data);
+            createDegreeInfoTable(data.degrees);
         })
         .catch(error => {
             console.log(error);
         });
 }
 
-const createHeader = (headerText, row) => {
-    let th = document.createElement('th');
-    let textNode = document.createTextNode(headerText)
-    th.appendChild(textNode)
-    return row.append(th)
+const createHeadersRow = (headers) => {
+    const thr = document.createElement('tr');
+    for (const header of headers) {
+        const th = document.createElement('th');
+        const textNode = document.createTextNode(header)
+        th.appendChild(textNode)
+        thr.append(th)
+    }
+    return thr
 }
 
-const createCell = (cellText, row) => {
-    let td = document.createElement('td');
-    let textNode = document.createTextNode(cellText)
-    td.appendChild(textNode)
-    return row.append(td)
+const createRow = (cells) => {
+    const tr = document.createElement('tr');
+    for (const cell of cells) {
+        const td = document.createElement('td');
+        const textNode = document.createTextNode(cell)
+        td.appendChild(textNode)
+        tr.append(td)
+    }
+    return tr
 }
 
-const createDegreeInfoTable = (data) => {
-    const headers = ['School', 'Program/Major', 'Type', 'Year']
+const createDegreeInfoTable = (degrees) => {
+    const table = document.createElement('table');
+    table.id = 'degrees-table'
 
-    let table = document.createElement('table');
-    table.classList.add('degrees-table');
-    let thr = document.createElement('tr');
+    // Define, create, and append the headers row to the table
+    const headersRow = createHeadersRow(['School', 'Program/Major', 'Type', 'Year', ]);
+    table.append(headersRow);
 
-    for (let header of headers) {
-        createHeader(header, thr)
+    // Iterate through degrees: define, create, and append rows to the table
+    for (const degree of degrees) {
+        const row = createRow([degree.school, degree.programMajor, degree.type, degree.year]);
+        table.append(row);
     }
 
-    table.append(thr)
+    const resetButton = document.getElementById('reset-button');
+    resetButton.insertAdjacentElement('afterend', table);
 
-    for (let degree of data.degrees) {
-
-        let tr = document.createElement('tr');
-
-        createCell(degree.school, tr)
-        createCell(degree.programMajor, tr)
-        createCell(degree.type, tr)
-        createCell(degree.year, tr)
-
-        table.append(tr)
-    }
-
-    const mainButton = document.getElementById('main-button');
-    mainButton.insertAdjacentElement('afterend', table);
+    document.getElementById('main-button').disabled = true;
+    document.getElementById('reset-button').disabled = false;
 };
 
+const resetDegreeInfo = () => {
+    document.getElementById('degrees-table').remove();
+    document.getElementById('main-button').disabled = false;
+    document.getElementById('reset-button').disabled = true;
+}
+
 document.getElementById('main-button').addEventListener('click', showDegreeInfo);
+document.getElementById('reset-button').addEventListener('click', resetDegreeInfo);
