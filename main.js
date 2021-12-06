f248cb512f13c2eb244213d64695460e57bfcfbf
Phil Gee <philgeejunk@gmail.com>
2021-12-06 18:58:40 -0500

    Final draft before validate

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ const showDegreeInfo = () => {
         .then(response => {
             if (response.ok) {
                 return response.json();
-            } else throw new Error('Fail');
+            } else {
+                throw new Error('Failed to load degrees.')
+            };
         })
-        .then(data => {
-            createDegreeInfoTable(data.degrees);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+        .then(data => createDegreeInfoTable(data.degrees))
+        .catch(error => reportError(error));
 }
 
+// creates and appends the header row with provided array of header text
 const createHeadersRow = (headers) => {
     const thr = document.createElement('tr');
     for (const header of headers) {
@@ -24,6 +23,7 @@ const createHeadersRow = (headers) => {
     return thr
 }
 
+// creates and appends a row of data with provided array of cells
 const createRow = (cells) => {
     const tr = document.createElement('tr');
     for (const cell of cells) {
@@ -35,9 +35,24 @@ const createRow = (cells) => {
     return tr
 }
 
+// Sends error message to DOM upon failed json fetch
+const reportError = (message) => {
+    const errorP = document.createElement('p');
+    errorP.id = 'degrees-section'
+    const textNode = document.createTextNode(message)
+    errorP.appendChild(textNode)
+
+    const resetButton = document.getElementById('reset-button');
+    resetButton.insertAdjacentElement('afterend', errorP);
+
+    document.getElementById('main-button').disabled = true;
+    document.getElementById('reset-button').disabled = false;
+}
+
+// creates degree table from json
 const createDegreeInfoTable = (degrees) => {
     const table = document.createElement('table');
-    table.id = 'degrees-table'
+    table.id = 'degrees-section'
 
     // Define, create, and append the headers row to the table
     const headersRow = createHeadersRow(['School', 'Program/Major', 'Type', 'Year', ]);
@@ -56,8 +71,9 @@ const createDegreeInfoTable = (degrees) => {
     document.getElementById('reset-button').disabled = false;
 };
 
+// returns to original/cleared state
 const resetDegreeInfo = () => {
-    document.getElementById('degrees-table').remove();
+    document.getElementById('degrees-section').remove();
     document.getElementById('main-button').disabled = false;
     document.getElementById('reset-button').disabled = true;
 }
